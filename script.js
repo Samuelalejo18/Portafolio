5d41696308a6437e874ad3093f6333c296db2382
Samuel Alejandro Monsalve Sarmiento <132483306+Samuelalejo18@users.noreply.github.com>
2023-10-22 23:16:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+document.getElementById('descargar').addEventListener('click', function() {
+    const url = './img/CV - Samuel Alejandro Monsalve Sarmiento.pdf'; // Cambia la URL por la ruta de tu archivo
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'CV - Samuel Alejandro Monsalve Sarmiento.pdf'; // Nombre del archivo que se descargará
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+});
+
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    const  skills = document.getElementById("skills");
+    const  distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
